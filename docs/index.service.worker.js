cea41a677b030f5a5e801367f63ac31d6e09fb79
night5hade <129759458+night5hade@users.noreply.github.com>
2025-01-03 10:47:36 -0500

    Typos

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1735877134|912096751';
+const CACHE_VERSION = '1735919174|2301658634';
 /** @type {string} */
 const CACHE_PREFIX = 'Isofar Companion-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
