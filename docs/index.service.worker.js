f2b3b09ddf928891fdbba6c62d5f3306a703a713
night5hade <129759458+night5hade@users.noreply.github.com>
2025-01-02 23:07:25 -0500

    updated Disruptor

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734704758|26468689';
+const CACHE_VERSION = '1735877134|912096751';
 /** @type {string} */
 const CACHE_PREFIX = 'Isofar Companion-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
